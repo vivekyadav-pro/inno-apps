a97164f7e3584bb779040e043b4361a74e178c60
VIVEK YADAV <VIVEKYADAV1412@GMAIL.COM>
2020-07-01 01:10:58 +0530

    Updates

---
+++ precache-manifest.19c591e08dacf249c181c72661213025.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e41b480763ea7ec7146b26cfb0bf9e2",
+    "url": "/inno-apps/index.html"
+  },
+  {
+    "revision": "19ed4d3fa11c8f4db42b",
+    "url": "/inno-apps/static/css/main.90fac511.chunk.css"
+  },
+  {
+    "revision": "b34da60f2d0256c3dfdb",
+    "url": "/inno-apps/static/js/2.2a4da1b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/inno-apps/static/js/2.2a4da1b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19ed4d3fa11c8f4db42b",
+    "url": "/inno-apps/static/js/main.f40ba7cf.chunk.js"
+  },
+  {
+    "revision": "34af52628e3f0cae58c0",
+    "url": "/inno-apps/static/js/runtime-main.28b43152.js"
+  }
+]);
